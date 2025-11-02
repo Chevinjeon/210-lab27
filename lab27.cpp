b61a18b7ca7e14d46f488fceb3333023d12ed400
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-11-02 10:19:15 -0800

    second commit

--- lab27.cpp
+++ lab27.cpp
@@ -3,3 +3,9 @@
 #include <tuple>
 #include <string>
 using namespace std;
+
+// Type alias for villager data: (friendship level, species, catchphrase)
+using VillagerData = tuple<int, string, string>;
+
+// Map: villager name -> (friendship level, species, catchphrase)
+map<string, VillagerData> villagers;
